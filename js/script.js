bdaa9ccce92311259475b082bf4c17a00de8c3b9
Omar Gonzalez <32150067+akirabushido@users.noreply.github.com>
2019-04-07 16:48:22 +0900

    added my conts

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
-
+const studentList = document.getElementsByTagName('li');
+const studentsPerPage = 10;
 
 
 /*** 
@@ -34,7 +34,9 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+const showPage = (list, page) => {
 
+}
 
 
 
